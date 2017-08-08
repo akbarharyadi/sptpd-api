dff7b3042ce89ba9d247b18a27da724f797d097a
Akbar Priyono Haryadi <akbarharyadi777@gmail.com>
2017-08-08 22:40:34 +0700

    add get taxes

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -58,4 +58,42 @@ class ApiController extends \yii\rest\Controller
     }
     return $response;
   }
+
+  public function actionGetTaxes()
+  {
+    $response = [];
+    $year = $_POST['year'];
+    if (empty($year)) {
+      $response = [
+        'status' => 'error',
+        'dataTaxes' => (array)$Ayat,
+        'msg' => 'Tahun tidak ada.',
+      ];
+    }
+    else {
+      $Ayat = \app\models\TAyat::find()
+        ->select('kd_ayt, nm_ayt')
+        ->where(['tahun' => $year, 'kl_ayt' => '00', 'jn_ayt' => '00'])
+        ->andWhere("substring(kd_ayt, 1, 2) = '11'")
+        ->andWhere(['not in','kd_ayt',['1113', '1114']])
+        ->orderBy(['kd_ayt' => \SORT_ASC])
+        ->asArray()
+        ->all();
+      if (empty($Ayat)) {
+        $response = [
+          'status' => 'error',
+          'dataTaxes' => null,
+          'msg' => 'Rekening pajak tidak ditemukan.',
+        ];
+      }
+      else {
+        $response = [
+          'status' => 'success',
+          'dataTaxes' => (array)$Ayat,
+          'msg' => 'Rekening pajak ditemukan.',
+        ];
+      }
+    }
+    return $response;
+  }
 }
